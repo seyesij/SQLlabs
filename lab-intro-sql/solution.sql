33b96bce8e102f1b88c65c90f47e6684fba870f6
adeseye sijuwade <adeseyesijuwade@gmail.com>
2023-04-06 13:59:03 +0100

    moved folder

---
+++ lab-intro-sql/solution.sql
@@ -0,0 +1,21 @@
+USE sakila;
+
+SELECT * FROM actor;
+
+SELECT * FROM film;
+
+SELECT * FROM customer;
+
+SELECT title FROM film;
+
+SELECT name AS language
+FROM language;
+
+SELECT count(store_id) FROM store;
+
+SELECT count(staff_id) FROM staff;
+
+SELECT first_name FROM staff;
+
+
+
